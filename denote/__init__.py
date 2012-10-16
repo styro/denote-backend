1dc553fe233992646099ddcc2d3330c2ea50c829
Anton D <anton@screwtop>
2012-10-16 21:55:35 +1300

    Make project look more like the pyramid alchemy scaffold.

--- denote/__init__.py
+++ denote/__init__.py
@@ -3,13 +3,10 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    _add_static_views(config)
     config.scan()
     return config.make_wsgi_app()
-
-def _add_static_views(config):
-    config.add_static_view(name='css', path='denote:static/css')
-    config.add_static_view(name='src', path='denote:static/src')
