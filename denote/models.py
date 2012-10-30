2ca57724f52ca45324803174fdbf0a8fe1c8e2b4
Anton D <anton@screwtop>
2012-10-30 22:07:11 +1300

    New model code

--- denote/models.py
+++ denote/models.py
@@ -1,7 +1,13 @@
+import datetime
+
 from sqlalchemy import (
+    Table,
     Column,
+    DateTime,
     Integer,
-    Text,
+    Unicode,
+    UnicodeText,
+    ForeignKey,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,6 +15,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -16,13 +23,48 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+note_label_table = Table('notes_labels', Base.metadata,
+    Column('note_id', Integer, ForeignKey('notes.id')),
+    Column('label_id', Integer, ForeignKey('labels.id'))
+)
+
+class Note(Base):
+    __tablename__ = 'notes'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+    title = Column(Unicode)
+    content = Column(UnicodeText)
+    created_on = Column(DateTime)
+    creator_id = Column(Integer, ForeignKey('users.id'))
+    labels = relationship("Label", secondary=note_label_table, backref="notes")
 
-    def __init__(self, name, value):
+    def __init__(self, title, content=None, creator=None):
+        self.title = title
+        self.created_on = datetime.datetime.utcnow()
+        if content:
+            self.content = content
+        if creator:
+            self.created_by = creator
+
+class Label(Base):
+    __tablename__ = 'labels'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, unique=True)
+
+    def __init__(self, name):
         self.name = name
-        self.value = value
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, unique=True)
+    notes = relationship("Note", backref="created_by")
+    identities = relationship("Identity", backref="user")
+
+    def __init__(self, name):
+        self.name = name
+
+class Identity(Base):
+    __tablename__ = 'identities'
+    id = Column(Integer, primary_key=True)
+    name = Column(UnicodeText, unique=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
--- denote/scripts/initializedb.py
+++ denote/scripts/initializedb.py
@@ -11,14 +11,17 @@ from pyramid.paster import (
 
 from ..models import (
     DBSession,
-    MyModel,
     Base,
+    Note,
+    User,
+    Label,
+    Identity,
     )
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
-          '(example: "%s development.ini")' % (cmd, cmd)) 
+          '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
 def main(argv=sys.argv):
@@ -31,5 +34,24 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        u1 = User('User One')
+        u2 = User('User Two')
+        DBSession.add_all([u1, u2])
+        i1 = Identity(name='identity1', user=u1)
+        i2 = Identity(name='identity2', user=u1)
+        i3 = Identity(name='identity3', user=u2)
+        DBSession.add_all([i1, i2, i3])
+        l1 = Label('first label')
+        l2 = Label('second label')
+        l3 = Label('third label')
+        DBSession.add_all([l1, l2, l3])
+        n1 = Note(title='First note', content='First note content\nsecond line')
+        n1.created_by = u1
+        n1.labels = [l1, l3]
+        n2 = Note(title='Second note', content='Second note content\nsecond line')
+        n2.created_by = u2
+        n2.labels = [l2, l3]
+        n3 = Note(title='Third note', content='Third note content\nsecond line')
+        n3.created_by = u1
+        n3.labels = [l1]
+        DBSession.add_all([n1, n2, n3])
